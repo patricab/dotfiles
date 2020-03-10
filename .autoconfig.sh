98cf8e86cb0a91400880e0231d06155202c8c66b
patricab <pigface.survey@gmail.com>
2020-03-10 12:56:06 +0100

    * autoconfig.sh - Removed unnecessary packages

--- .autoconfig.sh
+++ .autoconfig.sh
@@ -96,7 +96,6 @@ function APT() {
     sudo apt-get -y install slack-desktop
     sudo apt-get -y install libreoffice
     sudo apt-get -y install vim
-    sudo apt-get -y install vim-gtk
     sudo apt-get -y install vlc
     sudo apt-get -y install python3
     sudo apt-get -y install dos2unix
