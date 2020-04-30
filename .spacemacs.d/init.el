dd497ad1a4a0fc7a11ea9156061c30e36133bbd5
patricab <pigface.survey@gmail.com>
2020-04-30 11:23:03 +0200

    * Removed support for spell-checking

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -52,7 +52,7 @@ values."
      yaml
      ruby
      ranger
-     spell-checking
+     ;; spell-checking
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
