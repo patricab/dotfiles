fc5c66c92ddaf25f46b3e976feb59cbac31409e3
patricab <patric.berthelsen@gmail.com>
2020-11-10 09:38:24 +0100

    * Added 'shell' package - init.el

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     (shell :variables shell-default-shell 'term)
      pdf-tools
      csv
      html
