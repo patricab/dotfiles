2aa098244a851e4a3795469680531b6658d40253
patricab <patric.berthelsen@gmail.com>
2021-02-26 13:15:10 +0100

    Removed unnecessary config files

--- .newsbeuter/bookmark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-url="$1"
-title="$2"
-description="$3"
-feed_title="$4"
-
-echo -e "${url}\t${title}\t${description}\t${feed_title}" >> ~/.newsbeuter/bookmarks.txt
--- .touchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-
-if [ $(synclient -l | grep -c 'TouchpadOff.*=.*0') == "1" ];
-then
-	synclient TouchpadOff=1
-else
-	synclient TouchpadOff=0
-fi
