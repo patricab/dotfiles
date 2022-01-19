e1eff512a73f037ea76f6ebdd85bb5285c1f65d5
patricab <patric.berthelsen@gmail.com>
2022-01-19 15:22:10 +0100

    Added installation for Github CLI

--- .install.sh
+++ .install.sh
@@ -40,5 +40,13 @@ git config --global core.editor "code --wait"
 git config --global user.name "patricab"
 git config --global user.email patric.berthelsen@gmail.com
 
+# Github CLI (GH)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt -y update
+sudo apt -y install gh
+gh auth login
+gh conig set editor "code --wait"
+
 # Upgrade system and reboot
 sudo apt-get -y upgrade
