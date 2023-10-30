2db2a0fd0f1cfb2b49d98a1651e9dc9ffbb68a3b
patricab <patric.berthelsen@gmail.com>
2023-10-30 10:57:41 +0100

    feat: add new package, extended treesitter support

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -9,6 +9,11 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
 	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use({
+          "nvim-treesitter/nvim-treesitter-textobjects",
+          after = "nvim-treesitter",
+          requires = "nvim-treesitter/nvim-treesitter",
+    })
 	use {'nvim-treesitter/playground'}
 
     -- My boy
