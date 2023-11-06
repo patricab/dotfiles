742b5551ee413069579cad2b481937a5e603e2c6
patricab <patric.berthelsen@gmail.com>
2023-11-06 09:51:38 +0100

    fix: change install and settings for macbook

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -34,15 +34,16 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
 cmp.setup({
     sources = {
-        {name = 'path'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
+        {name = "path"},
+        {name = "nvim_lsp"},
+        {name = "nvim_lua"},
+        {name = "pylsp"},
     },
     formatting = lsp_zero.cmp_format(),
     mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-Space>"] = cmp.mapping.confirm({ select = true }),
+        -- [""] = cmp.mapping.complete(),
     }),
 })
